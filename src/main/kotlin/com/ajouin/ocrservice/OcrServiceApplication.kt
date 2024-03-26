0b61b7c48e7763defdfdcefff8de61d963c4e9bf
moomani <moonman@kakao.com>
2024-03-26 21:07:31 +0900

    init project

---
+++ src/main/kotlin/com/ajouin/ocrservice/OcrServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.ajouin.ocrservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OcrServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OcrServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/ajouin/ocrservice/OcrServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ajouin.ocrservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OcrServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
