30e2840eb77caa6c08cd893c50836d14faeca5cb
moomani <moonman@kakao.com>
2024-04-02 21:51:01 +0900

    add JSON annotations to dto for explicit mapping

--- src/main/kotlin/com/ajouin/ocrservice/dto/OcrRequest.kt
+++ src/main/kotlin/com/ajouin/ocrservice/dto/OcrRequest.kt
@@ -1,8 +1,10 @@
 package com.ajouin.ocrservice.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
-data class OcrRequest(
-    val id: UUID,
-    val imageUrl: List<String>,
+data class OcrRequest @JsonCreator constructor (
+    @JsonProperty("id") val id: UUID,
+    @JsonProperty("imageUrl") val imageUrl: List<String>,
 )
--- src/main/kotlin/com/ajouin/ocrservice/dto/OcrResponse.kt
+++ src/main/kotlin/com/ajouin/ocrservice/dto/OcrResponse.kt
@@ -1,8 +1,10 @@
 package com.ajouin.ocrservice.dto
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
-data class OcrResponse(
-    val id: UUID,
-    val content: List<String>,
+data class OcrResponse @JsonCreator constructor (
+    @JsonProperty("id") val id: UUID,
+    @JsonProperty("content") val content: List<String>,
 )
