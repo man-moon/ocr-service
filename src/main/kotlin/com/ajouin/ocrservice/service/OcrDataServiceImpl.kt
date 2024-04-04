9201a1394d322349c46287edc258c3bb5ffecc66
moomani <moonman@kakao.com>
2024-04-05 04:12:12 +0900

    add exception handling logic when using google vision api

--- src/main/kotlin/com/ajouin/ocrservice/service/OcrDataServiceImpl.kt
+++ src/main/kotlin/com/ajouin/ocrservice/service/OcrDataServiceImpl.kt
@@ -3,9 +3,11 @@ package com.ajouin.ocrservice.service
 import com.ajouin.ocrservice.dto.OcrRequest
 import com.ajouin.ocrservice.dto.OcrResponse
 import com.ajouin.ocrservice.event.OcrResponseCreatedEvent
+import com.ajouin.ocrservice.logger
 import com.ajouin.ocrservice.publisher.OcrResponseEventPublisher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import org.springframework.cloud.gcp.vision.CloudVisionException
 import org.springframework.cloud.gcp.vision.CloudVisionTemplate
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Service
@@ -27,8 +29,16 @@ class OcrDataServiceImpl(
     }
 
     override fun extractText(request: OcrRequest): OcrResponse {
-        val results: List<String> = request.imageUrl.map { url ->
-            cloudVisionTemplate.extractTextFromImage(resourceLoader.getResource(url))
+
+        val results: MutableList<String> = mutableListOf()
+
+        request.imageUrl.forEach { url ->
+            try {
+                val text = cloudVisionTemplate.extractTextFromImage(resourceLoader.getResource(url))
+                results.add(text)
+            } catch (e: CloudVisionException) {
+                logger.error { "OCR 에러 발생: ${e.message}, 해당 이미지는 건너뛰고 다음 이미지 진행" }
+            }
         }
 
         return OcrResponse(request.id, results)
